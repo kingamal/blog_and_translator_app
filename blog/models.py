f523120682cbb1d35ef3cff312acd840b24f95b6
Kinga <kinga.maleszewska@o2.pl>
2024-11-29 11:59:13 +0100

    Correcting misstape in model

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,5 @@ class Post(models.Model):
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, unique=True)
-    authot = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     status = models.IntegerField(choices=STATUS, default=0)
