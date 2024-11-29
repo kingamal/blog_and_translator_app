cd1c88fbc713fbb782b771638a69a995aecd2e16
Kinga <kinga.maleszewska@o2.pl>
2024-11-29 13:30:20 +0100

    Creating a form

--- translator/urls.py
+++ translator/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.translator_view(), name='translator_view'),
+    path('', views.translator_view, name='translator_view'),
 ]
